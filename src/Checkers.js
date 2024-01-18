8d856479a57d95c62bafb821246ba10085783252
donavonhayes <131027014+donavonhayes@users.noreply.github.com>
2024-01-18 11:27:04 -0600

    8 commit

--- src/Checkers.js
+++ src/Checkers.js
@@ -1,62 +1,286 @@
-// Checkers.js
+let board = [
+  [0, -1, 0, -1, 0, -1, 0, -1, 0, -1],
+  [-1, 0, -1, 0, -1, 0, -1, 0, -1, 0],
+  [0, -1, 0, -1, 0, -1, 0, -1, 0, -1],
+  [-1, 0, -1, 0, -1, 0, -1, 0, -1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+];
 
-import React, { useState } from 'react';
-import './index.css'; // Import your CSS file
+function builBoard() {
+  game.innerHTML = "";
+  
+  for (let i = 0; i < board.length; i++) {
+    const element = board[i];
+    let row = document.createElement("div"); // create div for each row
+    row.setAttribute("class", "row");
 
-const Checkers = () => {
-  const [board, setBoard] = useState([
-    ['black', null, 'black', null, null, null, 'red', null],
-    [null, 'black', null, 'black', null, 'red', null, 'red'],
-    ['black', null, 'black', null, null, null, 'red', null],
-    [null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null],
-    [null, 'red', null, 'red', null, 'black', null, 'black'],
-    ['red', null, 'red', null, null, null, 'black', null],
-    [null, 'red', null, 'red', null, 'black', null, 'black'],
-  ]);
+    for (let j = 0; j < element.length; j++) {
+      const elmt = element[j];
+      let col = document.createElement("div"); 
+      let piece = document.createElement("div");
+      let caseType = "";
+      let occupied = "";
 
-  const [selectedPiece, setSelectedPiece] = useState(null);
-
-  const handleSquareClick = (row, col) => {
-    if (selectedPiece) {
-      // Handle the move logic here
-      // For simplicity, let's assume legal moves are only diagonal and capturing
-      // You may need to implement the full rules of checkers
-
-      const isLegalMove = ''
-      if (isLegalMove) {
-        // Update the board state
-        const newBoard = /* Perform the move and update the board */
-        setBoard(newBoard);
-        setSelectedPiece(null);
+      if (i % 2 === 0) {
+        if (j % 2 === 0) {
+          caseType = "Whitecase";
+        } else {
+          caseType = "blackCase";
+        }
       } else {
-        // Inform the player that the move is not legal
-        alert('Illegal move. Try again.');
+        if (j % 2 !== 0) {
+          caseType = "Whitecase";
+        } else {
+          caseType = "blackCase";
+        }
       }
-    } else {
-      // Handle the selection logic here
-      const piece = board[row][col];
-      if (piece !== null /* Check if it's the player's piece */) {
-        setSelectedPiece({ row, col });
-      }
-    }
-  };
 
-  return (
-    <div className="checkers-board">
-      {board.map((row, rowIndex) => (
-        <div key={rowIndex} className="checkers-row">
-          {row.map((piece, colIndex) => (
-            <div
-              key={colIndex}
-              className={`checker ${piece}`}
-              onClick={() => handleSquareClick(rowIndex, colIndex)}
-            ></div>
-          ))}
-        </div>
-      ))}
-    </div>
-  );
-};
+      // add the piece if the case isn't empty
+      if (board[i][j] === 1) {
+        occupied = "whitePiece";
+      } else if (board[i][j] === -1) {
+        occupied = "blackPiece";
+      } else {
+        occupied = "empty";
+      }
+
+      piece.setAttribute("class", "occupied " + occupied);
+
+      // set row and colum in the case
+      piece.setAttribute("row", i);
+      piece.setAttribute("column", j);
+      piece.setAttribute("data-position", i + "-" + j);
+
+      //add event listener to each piece
+      piece.addEventListener("click", movePiece);
+
+      col.appendChild(piece);
+
+      col.setAttribute("class", "column " + caseType);
+      row.appendChild(col);
+      
+    }
+
+    game.appendChild(row);
+  }
+}
+
+let currentPlayer = 1;
+
+let newPiecesPositions = [];
+
+function movePiece(e) {
+  let piece = e.target;
+  const row = parseInt(piece.getAttribute("row"));
+  const column = parseInt(piece.getAttribute("column"));
+  let p = new Piece(row, column);
+
+  
+  if (newPiecesPosition.length > 0) {
+    enableToMove(p);
+  }
+
+  if (currentPlayer === board[row][column]) {
+    findPossibleNewPosition(p, player);
+  }
+}
+
+function findPossibleNewPosition(piece, player) {
+  if (board[piece.row + player][piece.column + 1] === 0) {
+    readyToMove = piece;
+    markPossiblePosition(piece, player, 1);
+  }
+
+  if (board[piece.row + player][piece.column - 1] === 0) {
+    readyToMove = piece;
+    markPossiblePosition(piece, player, -1);
+  }
+}
+
+function enableToMove(p) {
+  let found = false;
+  let newPosition = null;
+  
+  
+  posNewPosition.forEach((element) => {
+    if (element.compare(p)) {
+      found = true;
+      newPosition = element;
+      return;
+    }
+  });
+
+  if (found){
+     // if the current piece can move on, edit the board and rebuild
+    board[newPosition.row][newPosition.column] = currentPlayer;
+    board[readyToMove.row][readyToMove.column] = 0;
+
+    // init value
+    readyToMove = null;
+    posNewPosition = [];
+    capturedPosition = [];
+
+    currentPlayer = reverse(currentPlayer);
+
+    displayCurrentPlayer();
+    builBoard();
+  } else {
+    builBoard();
+   }
+}
+
+let piece = e.target;
+const row = parseInt(piece.getAttribute("row"));
+const column = parseInt(piece.getAttribute("column"));
+let p = new Piece(row, column);
+
+ if (posNewPosition.length > 0) {
+    enableToMove(p);
+ }
+
+function enableToMove(p) {
+  let find = false;
+  let newPosition = null;
+  // check if the case where the player play the selected piece can move on
+  posNewPosition.forEach((element) => {
+    if (element.compare(p)) {
+      find = true;
+      newPosition = element;
+      return;
+    }
+  });
+
+  if (find) moveThePiece(newPosition);
+  else builBoard();
+}
+
+function moveThePiece(newPosition) {
+  // if the current piece can move on, edit the board and rebuild
+  board[newPosition.row][newPosition.column] = currentPlayer;
+  board[readyToMove.row][readyToMove.column] = 0;
+
+  // init value
+  readyToMove = null;
+  posNewPosition = [];
+  capturedPosition = [];
+
+  currentPlayer = reverse(currentPlayer);
+
+  displayCurrentPlayer();
+  builBoard();
+}
+
+function findPieceCaptured(p, player) {
+  let found = false;
+  if (
+    board[p.row - 1][p.column - 1] === player &&
+    board[p.row - 2][p.column - 2] === 0
+  ) {
+    found = true;
+    newPosition = new Piece(p.row - 2, p.column - 2);
+    readyToMove = p;
+    markPossiblePosition(newPosition);
+    // save the new position and the opponent's piece position
+    capturedPosition.push({
+      newPosition: newPosition,
+      pieceCaptured: new Piece(p.row - 1, p.column - 1),
+    });
+  }
+
+  if (
+    board[p.row - 1][p.column + 1] === player &&
+    board[p.row - 2][p.column + 2] === 0
+  ) {
+    found = true;
+    newPosition = new Piece(p.row - 2, p.column + 2);
+    readyToMove = p;
+    markPossiblePosition(newPosition);
+    // save the new position and the opponent's piece position
+    capturedPosition.push({
+      newPosition: newPosition,
+      pieceCaptured: new Piece(p.row - 1, p.column + 1),
+    });
+  }
+
+  i
+
+ 
+  return found;
+}
+
+
+
+function enableToCapture(p) {
+  let find = false;
+  let pos = null;
+  capturedPosition.forEach((element) => {
+    if (element.newPosition.compare(p)) {
+      find = true;
+      pos = element.newPosition;
+      old = element.pieceCaptured;
+      return;
+    }
+  });
+
+  if (find) {
+    // if the current piece can move on, edit the board and rebuild
+    board[pos.row][pos.column] = currentPlayer; // move the piece
+    board[readyToMove.row][readyToMove.column] = 0; // delete the old position
+    // delete the piece that had been captured
+    board[old.row][old.column] = 0;
+
+    // reinit ready to move value
+
+    readyToMove = null;
+    capturedPosition = [];
+    posNewPosition = [];
+    displayCurrentPlayer();
+    builBoard();
+    // check if there are possibility to capture other piece
+    currentPlayer = reverse(currentPlayer);
+  } else {
+    builBoard();
+  }
+}
+
+
+if (board[i][j] === -1) {
+  blackPiecesCounter++;
+} else if (board[i][j] === 1) {
+  whitePiecesCounter++;
+}
+
+function displayCounter(blackPiecesCounter, whitePiecesCounter) {
+  var blackContainer = document.getElementById("black-player-count-pieces");
+  var whiteContainer = document.getElementById("white-player-count-pieces");
+  blackContainer.innerHTML = blackPiecesCounter;
+  whiteContainer.innerHTML = whitePiecesCounter;
+}
+
+function displayCurrentPlayer() {
+  let container = document.getElementById("next-player");
+  if (container.classList.contains("whitePiece")) {
+    container.setAttribute("class", "occupied blackPiece");
+  } else {
+    container.setAttribute("class", "occupied whitePiece");
+  }
+}
+if (black === 0 || white === 0) {
+  modalOpen(black);
+}
+
+
+function modalOpen(black) {
+document.getElementById("winner").innerHTML = black === 0 ? "White" : "Black";
+document.getElementById("loser").innerHTML = black !== 0 ? "White" : "Black";
+modal.classList.add("effect");
+}
 
 export default Checkers;
+
+
